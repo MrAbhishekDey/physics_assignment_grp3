f22b6d670f11e5d8a498fa03048b435145e71f93
prantodey108 <prantodey376@gmail.com>
2025-07-31 15:21:38 +0600

    changes made by mahin

--- problem2.c
+++ problem2.c
@@ -3,5 +3,24 @@
 
 int main() {
 
+    int m1 = 3;
+
+    double lambda1_nm = 530;  
+    double lambda1_m = lambda1_nm * 1e-9; // Convert nm to meters
+
+    double degree = 65.0;
+    double radian = degree * (3.1416 / 180.0);  // Convert degrees to radians
+
+    double sinTheta = sin(radian);   // Correct: use sin() function
+
+    double d = (m1 * lambda1_m) / sinTheta;
+
+    printf("d = %.10lf meters\n", d);
+
+
+
+
+
+    
     return 0;
 }
