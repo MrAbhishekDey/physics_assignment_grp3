56a43ca24d686429498672a342f4c6e71bace0c6
Abdul Muhaimin Labib <forgettingisacrime@gmail.com>
2025-07-31 23:01:39 +0600

    changes done by Abdul Muhaimin Labib

--- problem2.c
+++ problem2.c
@@ -2,10 +2,9 @@
 #include <math.h>
 
 int main() {
-
     int m1 = 3;
 
-    double lambda1_nm = 530;  
+    double lambda1_nm = 530;
     double lambda1_m = lambda1_nm * 1e-9; // Convert nm to meters
 
     double degree = 65.0;
@@ -15,12 +14,25 @@ int main() {
 
     double d = (m1 * lambda1_m) / sinTheta;
 
-    printf("d = %.10lf meters\n", d);
+    printf("d = %.10lf meters\n", d);  // Print with high precision
 
+  //.....Now.....//
 
+ int m2 = 2;
+double lambda2_nm = 700;
+double lambda2_m = lambda2_nm * 1e-9; // Convert nm to meters
 
+double sin_Theta2 = (m2 * lambda2_m) / d;
 
+    if (sin_Theta2 > 1.0)
+        sin_Theta2 = 1.0;  // Limit sin_Theta2 to 1.0
 
-    
-    return 0;
+    double radians = asin(sin_Theta2);
+
+    double theta2 =  radians * (180.0 / 3.1416);  // degree conversion
+
+    printf("Theta is : %lf", theta2);
+
+return 0;
+
 }
